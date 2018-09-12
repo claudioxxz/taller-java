50f9434eb1e585978da28284581deada6587409b
Claudio <claudio.gomezquidel@gmail.com>
2018-09-11 22:36:38 -0300

    Vista ingreso

---
+++ src/tallerjava/vistas/VistaIngreso.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tallerjava.vistas;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ElPCdeClaudio
+ */
+public class VistaIngreso extends JFrame{
+    public VistaIngreso(){
+        super("Ingreso");
+        
+    }
+}
