ca32db03686550867877438e1dfdfbb3a66be503
Javier Ortiz <Jatochi64@gmail.com>
2018-09-11 22:21:51 -0300

    refactorizado CuentaDao

--- src/tallerjava/dao/CuentaDAO.java
+++ src/tallerjava/dao/CuentaDAO.java
@@ -19,77 +19,58 @@ import tallerjava.modelo.Cuenta;
  */
 public class CuentaDAO {
     
-    Statement st;
+    Conexion con;
     
-    public CuentaDAO() throws ClassNotFoundException, SQLException{
-        try{
-            Conexion con = Conexion.getSingletonInstance();
-            Connection cn = con.getConnection();
-            st = cn.createStatement();
-        }catch(SQLException error){
-            throw new SQLException(error.getMessage()); 
-        }
-        
-        
+    public CuentaDAO(){
+        con = Conexion.getSingletonInstance();     
     }
     
     public int insertarCuenta(Cuenta cuenta) throws SQLException{
-        int filas = 0;
-        try{
-           filas = st.executeUpdate("Insert into cuenta values ('" + cuenta.getTipo() + "', " + cuenta.getSaldo() + ")");
-        }catch(SQLException error){
-            throw new SQLException(error.getMessage()); 
-        }
+        Connection cn = con.getConnection();
+        Statement st = cn.createStatement();
+        int filas = st.executeUpdate("Insert into cuenta values ('" + cuenta.getTipo() + "', " + cuenta.getSaldo() + ")");
+        cn.close();
         return filas;
     }
     
     public int editarCuenta(Cuenta cuenta) throws SQLException{
-        int filas = 0;
-        try{
-            filas = st.executeUpdate("update cuenta set tipo='" + cuenta.getTipo() + "', saldo=" + cuenta.getSaldo() + " where id=" +cuenta.getId() );
-        }catch(SQLException error){
-            throw new SQLException(error.getMessage());
-        }
+        Connection cn = con.getConnection();
+        Statement st = cn.createStatement();
+        int filas = st.executeUpdate("update cuenta set tipo='" + cuenta.getTipo() + "', saldo=" + cuenta.getSaldo() + " where id=" +cuenta.getId() );
+        cn.close();
         return filas;
     }
     
     public List<Cuenta> obtenerCuentas() throws SQLException{
-        
+        Connection cn = con.getConnection();
+        Statement st = cn.createStatement();
         ArrayList<Cuenta> listaCuenta = new ArrayList<>();
-        try{
-            ResultSet rs;
-            rs = st.executeQuery("Select * from cuenta");
-            while(rs.next()){
-                final Cuenta cuenta = new Cuenta(rs.getInt(1), rs.getString(2), rs.getDouble(3));
-                listaCuenta.add(cuenta);
-            }
-        }catch(SQLException error){
-            throw new SQLException(error.getMessage());
+        ResultSet rs;
+        rs = st.executeQuery("Select * from cuenta");
+        while(rs.next()){
+            final Cuenta cuenta = new Cuenta(rs.getInt(1), rs.getString(2), rs.getDouble(3));
+            listaCuenta.add(cuenta);
         }
+        cn.close();
         return listaCuenta;
     }
     
     public Cuenta obtenerCuentaPorId(int id) throws SQLException{
-        
+        Connection cn = con.getConnection();
+        Statement st = cn.createStatement();
         ResultSet rs;
-        try{
-            
-            rs = st.executeQuery("Select * from cuenta where id=" + id);
-        }catch(SQLException error){
-            throw new SQLException(error.getMessage());
-        }
+        rs = st.executeQuery("Select * from cuenta where id=" + id);
         rs.next();
         Cuenta cuenta = new Cuenta(rs.getInt(1), rs.getString(2), rs.getDouble(3));
+        cn.close();
         return cuenta;
     }
     
     public int borrarCuenta(int id) throws SQLException{
-        int filas = 0;
-        try{
-            filas = st.executeUpdate("delete cuenta where id=" + id);
-        }catch(SQLException error){
-            throw new SQLException(error.getMessage());
-        }
+        Connection cn = con.getConnection();
+        Statement st = cn.createStatement();
+        int filas = st.executeUpdate("delete cuenta where id=" + id);
+        cn.close();
         return filas;
     }
 }
